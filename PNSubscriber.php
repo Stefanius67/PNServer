3d930fb50748969b46061d7dd0e8c26fe6f99f9f
Stefan Kientzler <s.kien@online.de>
2020-08-08 00:57:44 +0200

    added Welcome Notification

--- PNSubscriber.php
+++ PNSubscriber.php
@@ -1,7 +1,34 @@
 <?php
 require_once 'autoloader.php';
+require_once 'PNSendWelcome.php';
 
 use SKien\PNServer\PNDataProviderSQLite;
+use SKien\PNServer\PNSubscription;
+
+/**
+ * Example to handle the HTTP-Request send from the ServiceWorker to 
+ * subscribe our notification service.
+ * 
+ * For easy setup we use the SQLite dataprovider and set the database file
+ * located in the same directory as this script.
+ * You may use the MySQL dataprovider, just locate the database in a differnet
+ * directory or write your own dataprovider for your project.
+ * 
+ * Remember to adjust the path and name of the script in the service worker
+ * (PNServiceWorker.js) when implementing the requesthandler in your own project.
+ * 
+ * Set $bSendWelcome=true, to send a welcome notification to each user 
+ * newly subscribed our service, after the subscription was saved in 
+ * the database.
+ *
+ * THIS CODE IS INTENDED ONLY AS EXAMPLE - DONT USE IT DIRECT IN YOU PROJECT
+ *
+ * @author Stefanius <s.kien@online.de>
+ * @copyright MIT License - see the LICENSE file for details
+ */
+
+// set to true, if you will send songle welcome notification to each new subscription
+$bSendWelcome = true;
 
 $result = array();
 // only serve POST request containing valid json data
@@ -14,6 +41,9 @@ if (strtolower($_SERVER['REQUEST_METHOD']) == 'post') {
 			$oDP = new PNDataProviderSQLite();
 			if ($oDP->saveSubscription($strJSON) !== false) {
 				$result['msg'] = 'subscription saved on server!';
+				if ($bSendWelcome) {
+				    sendWelcome(PNSubscription::fromJSON($strJSON));
+				}
 			} else {
 				$result['msg'] = 'error saving subscription!';
 			}
